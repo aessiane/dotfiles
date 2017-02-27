ecf5776d79b45eb38e92cffebfe6034d56bc775d
Essiane Abel <essiane.abel@gmail.com>
2017-02-27 23:25:04 +0000

    added uniform random

--- scripts/lock_screen.sh
+++ scripts/lock_screen.sh
@@ -1,13 +1,60 @@
 #!/bin/sh
 
+function uniform_random {
+  touch /tmp/uniform_random
+
+  NB_LAYERS=$1
+  FILES=( $(cat /tmp/uniform_random) )
+  EMPTY=0
+
+  if [ ${#FILES[@]} -eq $NB_LAYERS ];
+  then
+    rm /tmp/uniform_random
+    touch /tmp/uniform_random
+    EMPTY=1
+  fi
+
+  PRESENT=0
+  RAND=$(( $RANDOM % $NB_LAYERS ))
+
+  if [ $EMPTY -eq 1 ];
+  then
+    echo $RAND >> /tmp/uniform_random
+    echo $RAND
+    return
+  fi
+
+  while true
+  do
+    for i in ${FILES[@]}
+    do
+      if [ "$RAND" -eq "$i" ];
+      then
+        PRESENT=1
+        break
+      fi
+    done
+
+    if [ "$PRESENT" -eq "1" ];
+    then
+      RAND=$(( $RANDOM % $NB_LAYERS ))
+      PRESENT=0
+      continue
+    else
+      echo $RAND >> /tmp/uniform_random
+      echo $RAND
+      return
+    fi
+  done
+}
+
+
 TOP_DIRECTORY="$HOME/.wallpapers/overswag"
 UPPER_LAYERS=( $(ls "$TOP_DIRECTORY") )
-NB_LAYERS=${#UPPER_LAYERS[@]}
-RAND=$(echo "$RANDOM % $NB_LAYERS" | bc)
+
+RAND=$( uniform_random ${#UPPER_LAYERS[@]} )
 
 LAYER="$TOP_DIRECTORY/${UPPER_LAYERS[$RAND]}"
-
-echo $LAYER
 
 scrot /tmp/lock.png
 convert -blur 12x12 /tmp/lock.png /tmp/blurred.png
